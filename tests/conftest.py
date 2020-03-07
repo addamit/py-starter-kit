b7d87786def01be80412b8d12513fbaf0708390c
addamit <amitsharma103@gmail.com>
2020-03-07 14:12:39 -0500

    updated with test fixture for flask based service

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,20 @@ from pathlib import Path
 d = Path(__file__).resolve().parents[1]
 src_path = Path.joinpath(d, "src")
 sys.path.insert(0,  str(src_path))
+
+import pytest
+from appy import hello
+
+@pytest.fixture(scope='module')
+def test_client():
+	flask_app = hello.create_app()
+	flask_app.config['TESTING'] = True
+	testing_client = flask_app.test_client()
+	# Establish an application context before running the tests.
+	ctx = flask_app.app_context()
+	ctx.push()
+ 
+	yield testing_client  # this is where the testing happens!
+ 
+	ctx.pop()
+
