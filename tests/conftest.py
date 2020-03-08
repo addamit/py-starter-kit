f45dbd457bb5be6c9831959928a9616e0038ae27
addamit <amitsharma103@gmail.com>
2020-03-07 20:06:39 -0500

    updated with external api mock fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+
+from unittest.mock import Mock, patch
+
 import sys
 from pathlib import Path
 # `path.parents[1]` is the same as `path.parent.parent`
@@ -6,7 +9,10 @@ src_path = Path.joinpath(d, "src")
 sys.path.insert(0,  str(src_path))
 
 import pytest
+
 from appy import hello
+import appy
+
 
 @pytest.fixture(scope='module')
 def test_client():
@@ -20,4 +26,23 @@ def test_client():
 	yield testing_client  # this is where the testing happens!
  
 	ctx.pop()
+
+
+# below affects all tests in the test module
+#@pytest.fixture(scope='module')
+@pytest.fixture
+def mocked_external_get():
+
+	sample_data = {'data' : [1, 2, 3]}
+
+	mock_get_patcher = patch('appy.external_data.requests.get')
+	# start mocking
+	mock_get = mock_get_patcher.start()
+
+	mock_get.return_value = Mock()
+	mock_get.return_value.json.return_value = sample_data
+
+	yield mock_get
+
+	mock_get.stop()
 
